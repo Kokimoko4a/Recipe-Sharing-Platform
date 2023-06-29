d17f2e5decc6902447c4d3d19e6f4f2eed6d8610
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2023-06-29 16:30:34 +0300

    Added one stupid folder

---
+++ Recipe-Sharing-Platform/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Recipe_Sharing_Platform.Data;
+
+namespace Recipe_Sharing_Platform
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+
+    using Data;
+    using RecipeSharingPlatform.Data.Models;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<RecipeSharingPlatformDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 1;
+            })
+                .AddEntityFrameworkStores<RecipeSharingPlatformDbContext>();
+            builder.Services.AddControllersWithViews();
+
+            WebApplication app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapDefaultControllerRoute();
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
