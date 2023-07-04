fc2a32c7288821e3624219f37876d41b65d74737
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2023-07-04 10:00:14 +0300

    Changed the home page and started seeding the categories

---
+++ RecipeSharingPlatform.Data/Configurations/CategoryEntityConfiguration.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace RecipesSharingPlatform.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using RecipeSharingPlatform.Data.Models;
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Category[] GenerateCategories()
+        {
+            ICollection<Category> categories = new HashSet<Category>();
+
+            Category category = new Category()
+            {
+                Name = "Soups",
+                Id = 1
+            };
+
+            categories.Add(category);
+
+            category = new Category()
+            {
+                Id = 2,
+                Name = "Meals with pork"
+            };
+            
+            categories.Add(category);
+
+            category = new Category()
+            {
+                Id = 3,
+                Name = "Meals with lamb"
+            };
+
+            category = new Category()
+            {
+                Id = 4,
+                Name = "Meals with beef"
+            };
+        }
+    }
+}
