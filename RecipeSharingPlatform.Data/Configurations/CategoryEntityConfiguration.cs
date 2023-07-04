3a4b759230399235e76fa75a841c7e8ff118b929
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2023-07-04 14:23:28 +0300

    Seeded more data

--- RecipeSharingPlatform.Data/Configurations/CategoryEntityConfiguration.cs
+++ RecipeSharingPlatform.Data/Configurations/CategoryEntityConfiguration.cs
@@ -38,11 +38,93 @@ namespace RecipesSharingPlatform.Data.Configurations
                 Name = "Meals with lamb"
             };
 
+            categories.Add(category);.
+
             category = new Category()
             {
                 Id = 4,
                 Name = "Meals with beef"
             };
+
+            category = new Category()
+            {
+                Id = 5,
+                Name = "Meals with chicken"
+            };
+
+            category = new Category()
+            {
+                Id = 6,
+                Name = "Meals with rabbit meat"
+            };
+
+            category = new Category()
+            {
+                Id = 7,
+                Name = "Meals with duck meat"
+            };
+
+            category = new Category()
+            {
+                Id = 8,
+                Name = "Meals with goose turkey"
+            };
+
+            category = new Category()
+            {
+                Id = 9,
+                Name = "Desserts"
+            };
+
+            category = new Category()
+            {
+                Id = 10,
+                Name = "Meals with fish"
+            };
+
+            category = new Category()
+            {
+                Id = 11,
+                Name = "Seafood"
+            };
+
+            category = new Category()
+            {
+                Id = 12,
+                Name = "Meals with beef"
+            };
+
+            category = new Category()
+            {
+                Id = 13,
+                Name = "Pasta"
+            };
+
+            category = new Category()
+            {
+                Id = 14,
+                Name = "Garniture"
+            };
+
+            category = new Category()
+            {
+                Id = 15,
+                Name = "Vegetable meal"
+            };
+
+            category = new Category()
+            {
+                Id = 17,
+                Name = "Alcohol"
+            };
+
+            category = new Category()
+            {
+                Id = 16,
+                Name = "Cocktails"
+            };
+
+           
         }
     }
 }
