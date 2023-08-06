fc97ee3102a3b6a5a6016a2858572af800d4591a
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2023-08-06 12:04:05 +0300

    Cached more pages and changed some other things. Enjoy ;)

--- Recipe_Sharing_Platform/Controllers/CookingTypeController.cs
+++ Recipe_Sharing_Platform/Controllers/CookingTypeController.cs
@@ -3,23 +3,45 @@
 namespace Recipe_Sharing_Platform.Web.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Caching.Memory;
     using RecipeSharingPlatform.Services.Data.Interfaces;
     using RecipeSharingPlatform.Web.ViewModels.CookingType;
     using static RecipeSharingPlatform.Common.NotificationMessagesConstants;
+    using static RecipeSharingPlatform.Common.GeneralApplicationConstants;
 
     public class CookingTypeController : Controller
     {
         private readonly ICookingTypeService cookingTypeService;
+        private readonly IMemoryCache memoryCache;
 
-        public CookingTypeController(ICookingTypeService cookingTypeService)
+        public CookingTypeController(ICookingTypeService cookingTypeService,
+            IMemoryCache memoryCache)
         {
             this.cookingTypeService = cookingTypeService;
+            this.memoryCache = memoryCache;
         }
 
         [HttpGet]
         public async Task<IActionResult> ViewCookingTypes()
         {
-            return View(await cookingTypeService.GetAllCookingTypesAsViewModelsAsync());
+
+
+            IEnumerable<CookingTypeSmallViewModel> cookingTypeSmallViewModels = memoryCache.Get<IEnumerable<CookingTypeSmallViewModel>>(RecipesCacheKey);
+
+
+            if (cookingTypeSmallViewModels == null)
+            {
+                cookingTypeSmallViewModels = await cookingTypeService.GetAllCookingTypesAsViewModelsAsync();
+
+                MemoryCacheEntryOptions memoryCacheEntryOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(CacheRecipesDuration));
+
+                memoryCache.Set(RecipesCacheKey, cookingTypeSmallViewModels, memoryCacheEntryOptions);
+            }
+
+            return View(cookingTypeSmallViewModels);
+
+            
+
         }
 
         [HttpGet]
